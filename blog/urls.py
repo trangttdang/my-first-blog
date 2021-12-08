a15fa48ff29ae67e509b600fd587ffa6d5fa6587
trang <trangttdang01@gmail.com>
2021-12-08 15:41:08 -0500

    connect PostgreSQL DB and display Posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-from blog.views import PostViewSet
+# from blog.views import PostViewSet
 
-router = routers.DefaultRouter()
-router.register(r'post', PostViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'post', PostViewSet)
 
 urlpatterns = [
-  path('', include(router.urls)),
+  # path('', include(router.urls)),
   path('', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from blog.serializers import PostSerializer
 from blog.models import Post
 
-class PostViewSet(viewsets.ModelViewSet):
-  queryset = Post.objects.all()
-  serializer_class = PostSerializer
+from django.utils import timezone
+
+# class PostViewSet(viewsets.ModelViewSet):
+#   queryset = Post.objects.all()
+#   serializer_class = PostSerializer
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
