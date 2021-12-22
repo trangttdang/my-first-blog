934bd1b55df87a797c30ce1086a8d1b0b44c73d2
trang <trangttdang01@gmail.com>
2021-12-21 20:13:17 -0500

    display posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-# from blog.views import PostViewSet
+from blog.views import PostViewSet
 
-# router = routers.DefaultRouter()
-# router.register(r'post', PostViewSet)
+router = routers.DefaultRouter()
+router.register(r'post', PostViewSet)
 
 urlpatterns = [
-  # path('', include(router.urls)),
-  path('', views.post_list, name='post_list'),
+  path('', include(router.urls)),
+  path('post_list', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,13 @@ from blog.models import Post
 
 from django.utils import timezone
 
-# class PostViewSet(viewsets.ModelViewSet):
-#   queryset = Post.objects.all()
-#   serializer_class = PostSerializer
+class PostViewSet(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+  return render(request, 'blog/post_list.html', {'posts': PostViewSet.queryset}) 
